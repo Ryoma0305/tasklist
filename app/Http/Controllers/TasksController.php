7a6a2344a271bc476dd999c1cab16a05a01354ba
ryoma <techacademy.ryoma@gmail.com>
2020-02-22 10:30:21 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -44,7 +44,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required|max:191',   // 追加
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
 
@@ -96,7 +96,7 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'status' => 'required|max:191',   // 追加
+            'status' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
         
